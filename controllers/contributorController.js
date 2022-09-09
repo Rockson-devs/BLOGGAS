e3f01b097ac2a443d3a043794b867bdbb000f3dd
Rockson-devs <rockson.omane94@gmail.com>
2022-09-09 05:07:55 +0000

    Adding logic to controllers

--- controllers/contributorController.js
+++ controllers/contributorController.js
@@ -1,3 +1,8 @@
-module.exports = (req,res)=>{
-    res.render('contributor')
+const Blogpost = require('../model/blogpost')
+
+//this must be modified to display  only blogposts authored by the author who has logged in and only see all when he goes to index
+
+module.exports = async(req,res)=>{
+    const allBlogposts = await Blogpost.findOne({ })  //conditions set in the findOne method must be that it should be authored by that author
+    res.render('contributor', {allBlogposts: allBlogposts})
 }
---
+++ model/blogpost.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+
+const blogpostSchema = new mongoose.Schema({
+    blogTitle: {
+        type: String,
+        required: [true, 'please add a Title to this Blog']
+    },
+    blogDetails: {
+        type: String,
+        required: true
+    }
+});
+
+const Blogpost = mongoose.model('Blogpost', blogpostSchema)
+
+module.exports = Blogpost
